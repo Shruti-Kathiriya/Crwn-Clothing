137ce71b7f7bb3496106b223e31e5bdfcc21bac4
Shruti-Kathiriya <shruti.k@tiniyo.com>
2021-07-04 19:21:25 +0530

    created signin component with from input and custome button input

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './sign-in-and-sign-up.scss'
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInSignUP = () => {
+    return(
+        <div className='sign-in-and-sign-up'>
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInSignUP;
